c56aef064c995eeb39e9295d4b6852f74365da4e
Crhysss <crhystel.velasco@udla.edu.ec>
2023-06-05 16:39:11 -0500

    Más documentacion arreglada

--- prueba2.c
+++ prueba2.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+//variables que son las mismas en todas las funciones
 #define ALUMNOS 23
 #define PROGRESOS 3
 
-void obtener_promedio_alumnos(float calificaciones[][PROGRESOS], float promedios_alumnos[]);
-float obtener_promedio_progreso(float calificaciones[][PROGRESOS], int progreso);
+void obtener_promedio_alumnos(int calificaciones[][PROGRESOS], float promedios_alumnos[]);
+float obtener_promedio_progreso(int calificaciones[][PROGRESOS], int progreso);
 int obtener_alumno_mayor_promedio(float promedios_alumnos[]);
 
 int main() {
-    //definicion de los valores de cada estudiantes divididos en progresos
-    float calificaciones[ALUMNOS][PROGRESOS] = {
-        {1, 10, 8,9}, {2, 9.7, 7.5,8}, {3, 8.5, 9,4}, {4, 10, 9,6}, {5, 7.8, 7.5,5}, {6, 8.9, 9,10}, {7, 4, 5,6}, {8, 4.8, 6,9},
-        {9, 3.7, 4.5,7.9}, {10, 10, 9.8,6.9}, {11, 8.7, 9,10}, {12, 5.8, 6,1.3}, {13, 9.7, 10,5.8}, {14, 7.9, 8.5,5.8}, {15, 5.9, 7,9},
-        {16, 7, 7,9.7}, {17, 6, 6.5,6,7}, {18, 8.8, 8,10}, {19, 9.8, 9,3}, {20, 10, 10,10}, {21, 7.9, 8.5,10}, {22, 5.8, 7,10}, {23, 3.4, 4.5,8.9}
+    // Definición de los valores de cada estudiante divididos en progresos
+    int calificaciones[ALUMNOS][PROGRESOS] = {
+        {1, 10, 8}, {2, 9, 7}, {3, 8, 9}, {4, 10, 9}, {5, 7, 7}, {6, 8, 9}, {7, 4, 5}, {8, 4, 6},
+        {9, 3, 4}, {10, 10, 9}, {11, 8, 9}, {12, 5, 6}, {13, 9, 10}, {14, 7, 8}, {15, 5, 7},
+        {16, 7, 7}, {17, 6, 6}, {18, 8, 8}, {19, 9, 9}, {20, 10, 10}, {21, 7, 8}, {22, 5, 7}, {23, 3, 4}
     };
     float promedios_alumnos[ALUMNOS];
     float promedios_progresos[PROGRESOS];
@@ -30,7 +30,7 @@ int main() {
     // Obtener el alumno que obtuvo el mayor promedio de calificación durante el semestre
     alumno_mayor_promedio = obtener_alumno_mayor_promedio(promedios_alumnos);
 
-    // Imprimir resultados
+    // Imprimir lo requerido
     printf("Promedio de calificaciones de cada alumno:\n");
     for (int i = 0; i < ALUMNOS; i++) {
         printf("Alumno %d: %.2f\n", i + 1, promedios_alumnos[i]);
@@ -46,22 +46,19 @@ int main() {
     return 0;
 }
 
-void obtener_promedio_alumnos(float calificaciones[][PROGRESOS], float promedios_alumnos[]) {
-    int promedio;
+void obtener_promedio_alumnos(int calificaciones[][PROGRESOS], float promedios_alumnos[]) {
     for (int i = 0; i < ALUMNOS; i++) {
         float suma = 0.0;
         for (int j = 0; j < PROGRESOS; j++) {
             suma += calificaciones[i][j];
         }
-        //en teoria esta parte divide la suma para la candidad de progresos
-        promedios_alumnos[i] = suma / PROGRESOS;
-        promedio=promedios_alumnos[i]/3;
+        //se supone que con esto el promedio no deberia dar mayor de 10 T_T
+        promedios_alumnos[i] = (suma / PROGRESOS);
     }
 }
 
-float obtener_promedio_progreso(float calificaciones[][PROGRESOS], int progreso) {
+float obtener_promedio_progreso(int calificaciones[][PROGRESOS], int progreso) {
     float suma = 0.0;
-    //nota promedio de cada estudiante por progreso
     for (int i = 0; i < ALUMNOS; i++) {
         suma += calificaciones[i][progreso];
     }
@@ -69,7 +66,7 @@ float obtener_promedio_progreso(float calificaciones[][PROGRESOS], int progreso)
 }
 
 int obtener_alumno_mayor_promedio(float promedios_alumnos[]) {
-    //se realiza una comparacion entre los alumnos con mayor nota
+    //comparacion entre las notas de cada estudiante para saber cual es el que tiene mejor nota
     float mayor_promedio = promedios_alumnos[0];
     int indice_alumno_mayor_promedio = 0;
     for (int i = 1; i < ALUMNOS; i++) {
